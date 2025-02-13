a953c27bd9564155a481a171f78291d416d31010
Orlin Dimitrov <orlin369@gmail.com>
2025-02-13 13:26:42 +0200

    experiment launch file

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -60,18 +60,18 @@ def generate_launch_description():
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
-        name='joint_state_publisher',
-        parameters=[urdf_model_path],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        # name='joint_state_publisher',
+        parameters=[urdf_model_path]
+        # condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
 
     #
     joint_state_publisher_gui_node = launch_ros.actions.Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        parameters=[urdf_model_path],
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+        # name='joint_state_publisher_gui',
+        parameters=[urdf_model_path]
+        # condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
 
     #
