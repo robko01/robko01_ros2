27518188e983ff0e02aa04c7d8d7c96c9ccac61e
Orlin Dimitrov <orlin369@gmail.com>
2025-02-13 12:05:42 +0200

    Cleanup the launch file and add coments

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -4,21 +4,34 @@ import launch_ros
 import os
 
 def generate_launch_description():
-    pkgPath = launch_ros.substitutions.FindPackageShare(package='robko01_ros2').find('robko01_ros2')
-    urdfModelPath= os.path.join(pkgPath, 'urdf/robko01.urdf')
+
+    # Path to the package.
+    package_path = launch_ros.substitutions.FindPackageShare(package='robko01_ros2').find('robko01_ros2')
+
+    # Path to the URDF model.
+    urdf_model       = os.path.join(package_path, 'urdf/robko01.urdf')
+
+    # Path to the RViz configuration.
+    rviz_config_file = os.path.join(package_path, 'rviz', 'default.rviz')
+
+    # Load the URFT model.
+    robot_desc = b""
+    with open(urdf_model,'r') as infp:
+        robot_desc = infp.read()
     
-    with open(urdfModelPath,'r') as infp:
-    	robot_desc = infp.read()
+    # Parameters (robot model)
+    params = {
+        'robot_description': robot_desc
+    }
     
-    params = {'robot_description': robot_desc}
-    
+    #
     robot_state_publisher_node =launch_ros.actions.Node(
     package='robot_state_publisher',
 	executable='robot_state_publisher',
     output='screen',
     parameters=[params])
     
-    
+    #
     joint_state_publisher_node = launch_ros.actions.Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
@@ -26,6 +39,8 @@ def generate_launch_description():
         parameters=[params],
         condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
+
+    #
     joint_state_publisher_gui_node = launch_ros.actions.Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
@@ -33,11 +48,6 @@ def generate_launch_description():
         condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
     )
 
-    rviz_config_file = os.path.join(
-        pkgPath,
-        'rviz',
-        'default.rviz'
-    )
 
     rviz_node = launch_ros.actions.Node(
         package='rviz2',
@@ -50,7 +60,7 @@ def generate_launch_description():
     return launch.LaunchDescription([
         launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
                                             description='This is a flag for joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=urdfModelPath,
+        launch.actions.DeclareLaunchArgument(name='model', default_value=urdf_model,
                                             description='Path to the urdf model file'),
         robot_state_publisher_node,
         joint_state_publisher_node,
