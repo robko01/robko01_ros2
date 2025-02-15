4b7c2a87350b175275a080f12bb83454fb3c732d
Orlin Dimitrov <orlin369@gmail.com>
2025-02-15 20:23:51 +0200

    Test 2

--- robko01_ros2/joint_states_listener.py
+++ robko01_ros2/joint_states_listener.py
@@ -104,7 +104,7 @@ class JointStatesListener(Node):
         """Subscription instance.
         """        
 
-        self.__prescale_count = 30
+        self.__prescale_count = 20
 
 
         self.__prescale_counter = self.__prescale_count
@@ -199,7 +199,7 @@ class JointStatesListener(Node):
             self.__logger.error(traceback.format_exc())
 
     def __init_action_timer(self):
-        self.__action_update_timer.update_rate = 0.5
+        self.__action_update_timer.update_rate = 1000
         self.__action_update_timer.set_cb(self.__action_timer_cb)
         self.__action_update_timer.start()
 
@@ -218,11 +218,11 @@ class JointStatesListener(Node):
             self.__prescale_counter -= 1
         else:
             self.__prescale_counter == self.__prescale_count
-        angles = msg.position[0:6]
-        steps = self.__radians_to_steps(angles)
-        self.__set_position[0:12:2] = steps
-        self.__set_position[1:12:2] = [50, 50, 50, 50, 50, 50]
-        self.__put_action(Actions.UpdateAbsolutePositions)
+            angles = msg.position[0:6]
+            steps = self.__radians_to_steps(angles)
+            self.__set_position[0:12:2] = steps
+            self.__set_position[1:12:2] = [50, 50, 50, 50, 50, 50]
+            self.__put_action(Actions.UpdateAbsolutePositions)
 
     def __init_joint_listener(self):
         # Subscription
