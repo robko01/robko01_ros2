0805498838549c9746eeb3740796a041ad1d1dc8
Orlin Dimitrov <orlin369@gmail.com>
2025-02-16 16:32:04 +0200

    Change scaling coefficients

--- robko01_ros2/joint_states_listener.py
+++ robko01_ros2/joint_states_listener.py
@@ -57,7 +57,7 @@ class JointStatesListener(Node):
         self.__logger.info("HOI -> Human Oral Interaction")
 
         # self.__conversion_table_rad = [1125, 1125, 672, 241, 241, 1] # Original
-        self.__conversion_table_rad = [544, 544, 325, 150, 150, 1] # Compensated because of the motors controllers settings.
+        self.__conversion_table_rad = [544, 544, 325, 130, 130, 1] # Compensated because of the motors controllers settings.
         """Conversion tables from radians to steps.
         """
 
