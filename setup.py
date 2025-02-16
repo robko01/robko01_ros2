052b6cb1e70517e68afa1262e3a79693a0a350f6
Orlin Dimitrov <orlin369@gmail.com>
2025-02-16 19:46:54 +0200

    Update setup file

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/display.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/gazebo.launch.py']),
         ('share/' + package_name + '/urdf', ['urdf/robko01.urdf']),
         ('share/' + package_name + '/rviz', ['rviz/default.rviz']),
     ],
