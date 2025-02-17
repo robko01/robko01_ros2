48d77e647d06ba338309a2243f4f8a54c13863ef
Orlin Dimitrov <orlin369@gmail.com>
2025-02-17 22:18:05 +0200

    Test 4

--- robko01_ros2/joint_states_listener.py
+++ robko01_ros2/joint_states_listener.py
@@ -212,8 +212,13 @@ class JointStatesListener(Node):
         speeds = steps
         max_pos = max(steps)
         min_pos = min(steps)
+        if max_pos <= 0:
+            max_pos = 1
+        if min_pos <= 0:
+            min_pos = 1
         for index, step in enumerate(steps):
             speeds[index] = scale(speeds[index], min_pos, max_pos, 5, 50)
+            # speeds[index] = (steps[index] * speed) / max_pos
             speeds[index] = abs(speeds[index])
             speeds[index] = int(speeds[index])
             
