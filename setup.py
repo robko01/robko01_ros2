c71c7850656e278e25c83130105598771a411894
Orlin Dimitrov <orlin369@gmail.com>
2025-02-18 00:08:29 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/*.py']),
+        ('share/' + package_name + '/launch', ['launch/demo_launch.py', 'launch/gazebo_launch.py', 'launch/rviz_launch.py']),
         ('share/' + package_name + '/urdf', ['urdf/*.urdf']),
         ('share/' + package_name + '/rviz', ['rviz/*.rviz']),
         ('share/' + package_name + '/meshes', ['meshes/*.STL']),
