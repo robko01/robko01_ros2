85ef8dcd46ce8360370716073303b76095caaef6
Orlin Dimitrov <orlin369@gmail.com>
2025-02-25 09:58:16 +0200

    Fix setup file

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', [os.path.join("launch/", f) for f in os.listdir("./meshes/") if f.endswith(('.py'))]),
+        ('share/' + package_name + '/launch', [os.path.join("launch/", f) for f in os.listdir("./launch/") if f.endswith(('.py'))]),
         ('share/' + package_name + '/urdf', ['urdf/robko01.urdf']),
         ('share/' + package_name + '/rviz', ['rviz/default.rviz']),
         ('share/' + package_name + '/meshes', [os.path.join("meshes/", f) for f in os.listdir("./meshes/") if f.endswith(('.STL'))]),
