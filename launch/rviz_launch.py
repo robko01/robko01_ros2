be6c33283a6e043d5654056b01341f07e8959393
Orlin Dimitrov <orlin369@gmail.com>
2025-05-15 11:18:10 +0300

    Improve RViz launch

--- launch/rviz_launch.py
+++ launch/rviz_launch.py
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
+import tempfile
+
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, TimerAction
 from launch.conditions import IfCondition, UnlessCondition
@@ -28,66 +30,79 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
+import xacro
+
 def generate_launch_description():
 
     # Path to the package.
-    package_name = 'robko01_ros2'
+    package_name = "robko01_ros2"
     package_path = FindPackageShare(package=package_name).find(package_name)
 
     # Path to the URDF model.
-    urdf_model_path = os.path.join(package_path, 'urdf', 'robko01.urdf')
+    # urdf_model_path = os.path.join(package_path, "urdf", "robko01.urdf")
+    robot_model_path = os.path.join(package_path, "description", "robko01.urdf.xacro")
 
-    # Path to the RViz configuration.
-    rviz_config_path = os.path.join(package_path, 'rviz', 'default.rviz')
+    # Process xacro to URDF
+    robot_description = xacro.process_file(robot_model_path).toxml()
+
+    # Write to temporary file
+    # tmp_urdf = tempfile.NamedTemporaryFile(delete=False, suffix=".urdf")
+    # tmp_urdf.write(robot_description.encode("utf-8"))
+    # tmp_urdf.close()
 
     # Load the URDF model.
-    with open(urdf_model_path, 'r') as urdf_file:
-        urdf_model_content = urdf_file.read()
+    # urdf_model_content = None
+    # with open(tmp_urdf, "r") as urdf_file:
+    #     urdf_model_content = urdf_file.read()
 
-    # Parameters for nodes
-    params = {'robot_description': urdf_model_content}
-
-    # Nodes
-
-    joint_state_publisher_gui_node = TimerAction(
+    # Joint state publisher GUI
+    joint_state_publisher_gui = TimerAction(
         period=0.0,  # Delay to ensure robot_description is published
         actions=[
             Node(
-                package='joint_state_publisher_gui',
-                executable='joint_state_publisher_gui',
-                parameters=[urdf_model_path],
-                condition=IfCondition(LaunchConfiguration('gui'))
+                package="joint_state_publisher_gui",
+                executable="joint_state_publisher_gui",
+                parameters=[robot_model_path],
+                condition=IfCondition(LaunchConfiguration("gui"))
             )
         ]
     )
 
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        parameters=[urdf_model_path],
-        condition=UnlessCondition(LaunchConfiguration('gui'))
+    # Joint state publisher
+    joint_state_publisher = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        parameters=[robot_model_path],
+        condition=UnlessCondition(LaunchConfiguration("gui"))
     )
 
-    robot_state_publisher_node = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[params]
+    # Robot state publisher
+    robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[{
+            "robot_description": robot_description
+            }]
     )
 
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments=['-d', rviz_config_path],
-        output='screen'
+    # RViz
+    rviz_config_path = os.path.join(package_path, "rviz", "default.rviz")
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        arguments=["-d", rviz_config_path],
+        output="screen"
     )
 
     return LaunchDescription([
-        DeclareLaunchArgument(name='gui', default_value='True',
-                              description='Flag to enable joint_state_publisher_gui'),
-        joint_state_publisher_gui_node,
-        joint_state_publisher_node,
-        robot_state_publisher_node,
-        rviz_node
+        DeclareLaunchArgument(
+            name="gui",
+            default_value="True",
+            description="Flag to enable joint_state_publisher_gui"),
+        joint_state_publisher_gui,
+        joint_state_publisher,
+        robot_state_publisher,
+        rviz
     ])
